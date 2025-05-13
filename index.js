2c248ec43275aab8ce50599afc0440ae1c6464e9
Emad777 <sfa7.355@gmail.com>
2025-05-14 00:38:54 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-module.exports = require('./build/detect-and-parse.js');
+// index.js
+"use strict";
+
+// Import the main function and the SVM module (which now includes setModelPaths)
+const detectAndParseMrz = require("./build/detect-and-parse.js");
+const svm = require("./build/svm.js");
+
+// Attach the setModelPaths function to the exported function.
+detectAndParseMrz.setModelPaths = svm.setModelPaths;
+
+module.exports = detectAndParseMrz;
